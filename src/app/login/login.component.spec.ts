7ad3f8f8f6a1218934dd04daa465f3b004cb1533
Gursimran Singh <GSingh@mindtree_odc01.com>
2018-05-06 14:01:29 +0530

    added test cases

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,18 +1,53 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { DebugElement } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { BrowserModule, By } from '@angular/platform-browser';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule
+      ]
     })
-    .compileComponents();
+    .compileComponents().then(() => {
+      fixture = TestBed.createComponent(LoginComponent);
+      component = fixture.componentInstance;
+      de = fixture.debugElement.query(By.css('form'));
+      el = de.nativeElement;
+    });
   }));
 
+  it('from should be invalid' , async(() => {
+    component.LoginForm.controls['username'].setValue('');
+    component.LoginForm.controls['password'].setValue('');
+
+    expect(component.LoginForm.valid).toBeFalsy();
+  })
+);
+
+it('from should be valid' , async(() => {
+  component.LoginForm.controls['username'].setValue('gursimran');
+  component.LoginForm.controls['password'].setValue('something');
+
+  expect(component.LoginForm.valid).toBeTruthy();
+})
+);
+
+it('should have one user' , async(() => {
+  expect(component.user.email);
+  expect(component.user.password);
+}));
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
--- src/app/sign-up/sign-up.component.spec.ts
+++ src/app/sign-up/sign-up.component.spec.ts
@@ -19,6 +19,24 @@ describe('SignUpComponent', () => {
     fixture.detectChanges();
   });
 
+  it('from should be invalid' , async(() => {
+    component.signUpForm.controls['username'].setValue('');
+    component.signUpForm.controls['email'].setValue('');
+    component.signUpForm.controls['password'].setValue('');
+    component.signUpForm.controls['rePassword'].setValue('');
+    expect(component.signUpForm.valid).toBeFalsy();
+  })
+);
+
+it('from should be valid' , async(() => {
+  component.signUpForm.controls['username'].setValue('something');
+    component.signUpForm.controls['email'].setValue('something@example.com');
+    component.signUpForm.controls['password'].setValue('anmol');
+    component.signUpForm.controls['rePassword'].setValue('anmol');
+    expect(component.signUpForm.valid).toBeTruthy();
+})
+);
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
